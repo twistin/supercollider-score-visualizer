03ac49bf4ae48584773aca7196903687bf15694b
sdCarr <sarerac@gmail.com>
2025-07-08 19:09:30 +0200

    Initial commit

---
+++ components/icons/ClipboardDocumentCheckIcon.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export const ClipboardDocumentCheckIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M10.125 2.25h-4.5c-1.125 0-2.062.938-2.062 2.063v15.374c0 1.125.938 2.063 2.063 2.063h12.187c1.125 0 2.063-.938 2.063-2.063V8.625c0-1.125-.938-2.063-2.063-2.063h-4.5M13.875 2.25V5.25A2.625 2.625 0 0 0 16.5 7.875h2.625M10.125 2.25A2.625 2.625 0 0 1 12.75 4.875v.008c0 .017.004.034.004.051a2.625 2.625 0 0 1-5.25 0V4.875c0-.017.004-.034.004-.051a2.625 2.625 0 0 1 2.625-2.625Zm-1.875 9.375 1.5 1.5 3-3" />
+  </svg>
+);
